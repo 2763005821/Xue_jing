18ccc277e29974f0ca1a17c2a55ebd891e701cd4
2763005821 <2763005821@qq.com>
2024-04-28 19:14:01 +0800

    更新20240428.2

---
+++ public/pages/pwd.js
@@ -0,0 +1,12 @@
+function generatePassword(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let password = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+  
+  return password;
+}
+console.log(generatePassword(8));
